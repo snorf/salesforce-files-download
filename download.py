a9d1b5dcf64703441f6bcc8f181293ae7e9134c7
Johan Karlsteen <johan.karlsteen@gmail.com>
2020-03-19 13:33:15 +0100

    Changed from print to logging, loglevel is configurable in ini-file Using ProcessPoolExecutor to run downloads in parallell

--- download.py
+++ download.py
@@ -1,7 +1,9 @@
+import concurrent.futures
 from simple_salesforce import Salesforce
 import requests
 import os.path
 import csv
+import logging
 
 
 def split_into_batches(l, n):
@@ -47,53 +49,48 @@ def get_content_document_ids(sf, output_directory, query):
     return content_document_ids
 
 
-def fetch_files(sf, query_string, output_directory, valid_content_document_ids=None):
+def download_file(args):
+    record, output_directory, sf = args
+    filename = create_filename(record["Title"], record["FileExtension"], record["ContentDocumentId"], output_directory)
+    url = "https://%s%s" % (sf.sf_instance, record["VersionData"])
+
+    logging.debug("Downloading from " + url)
+    response = requests.get(url, headers={"Authorization": "OAuth " + sf.session_id,
+                                          "Content-Type": "application/octet-stream"})
+
+    if response.ok:
+        # Save File
+        with open(filename, "wb") as output_file:
+            output_file.write(response.content)
+        return "Saved file to %s" % filename
+    else:
+        return "Couldn't download %s" % url
+
+
+def fetch_files(sf, query_string, output_directory, valid_content_document_ids=None, batch_size=100):
     # Divide the full list of files into batches of 100 ids
-    batches = list(split_into_batches(valid_content_document_ids, 100))
+    batches = list(split_into_batches(valid_content_document_ids, batch_size))
 
     i = 0
     for batch in batches:
 
         i = i + 1
-        print('')
-        print("Processing batch {0}".format(i))
+        logging.info("Processing batch {0}/{1}".format(i, len(batches)))
         batch_query = query_string + ' AND ContentDocumentId in (' + ",".join("'" + item + "'" for item in batch) + ')'
         query_response = sf.query(batch_query)
-        batch_size = len(query_response["records"])
-        print("Content Version Query found {0} results".format(batch_size))
+        records_to_process = len(query_response["records"])
+        logging.debug("Content Version Query found {0} results".format(records_to_process))
 
         while query_response:
-
             extracted = 0
+            with concurrent.futures.ProcessPoolExecutor() as executor:
+                args = ((record, output_directory, sf) for record in query_response["records"])
+                for result in executor.map(download_file, args):
+                    logging.debug(result)
+            break
 
-            for r in query_response["records"]:
-
-                # Create filename
-                filename = create_filename(r["Title"], r["FileExtension"], r["ContentDocumentId"], output_directory)
-
-                # Download file
-                url = "https://%s%s" % (sf.sf_instance, r["VersionData"])
-                print("Downloading from " + url)
-                response = requests.get(url, headers={"Authorization": "OAuth " + sf.session_id,
-                                                      "Content-Type": "application/octet-stream"})
-
-                if response.ok:
-                    # Save File
-                    with open(filename, "wb") as output_file:
-                        output_file.write(response.content)
-                    print("Saved file to %s" % filename)
-                else:
-                    print("Couldn't download %s" % url)
-
-                extracted += 1
-                print("{0}/{1} complete".format(extracted, batch_size))
-
-            break;
-
-        print('All files in batch {0} downloaded'.format(i))
-        print('')
-
-    print('All batches complete')
+        logging.debug('All files in batch {0} downloaded'.format(i))
+    logging.debug('All batches complete')
 
 
 def main():
@@ -112,7 +109,11 @@ def main():
     username = config['salesforce']['username']
     password = config['salesforce']['password']
     token = config['salesforce']['security_token']
+    batch_size = int(config['salesforce']['batch_size'])
     is_sandbox = config['salesforce']['connect_to_sandbox']
+    loglevel = logging.getLevelName(config['salesforce']['loglevel'])
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=loglevel)
+
     content_document_query = 'SELECT ContentDocumentId, LinkedEntityId, ContentDocument.Title, ContentDocument.FileExtension FROM ContentDocumentLink WHERE LinkedEntityId in ({0})'.format(
         args.query)
     output = config['salesforce']['output_dir']
@@ -123,25 +124,25 @@ def main():
         domain = 'test';
 
     # Output
-    print('Export ContentVersion (Files) from Salesforce')
-    print('Username: ' + username)
-    print('Output directory: ' + output)
+    logging.info('Export ContentVersion (Files) from Salesforce')
+    logging.info('Username: ' + username)
+    logging.info('Output directory: ' + output)
 
     # Connect
     sf = Salesforce(username=username, password=password, security_token=token, domain=domain)
-    print("Connected successfully to {0}".format(sf.sf_instance))
+    logging.debug("Connected successfully to {0}".format(sf.sf_instance))
 
     # Get Content Document Ids
-    print("Querying to get Content Document Ids...")
+    logging.debug("Querying to get Content Document Ids...")
     valid_content_document_ids = None
     if content_document_query:
         valid_content_document_ids = get_content_document_ids(sf=sf, output_directory=output,
                                                               query=content_document_query)
-    print("Found {0} total files".format(len(valid_content_document_ids)))
+    logging.info("Found {0} total files".format(len(valid_content_document_ids)))
 
     # Begin Downloads
     fetch_files(sf=sf, query_string=query, valid_content_document_ids=valid_content_document_ids,
-                output_directory=output)
+                output_directory=output, batch_size=batch_size)
 
 
 if __name__ == "__main__":
